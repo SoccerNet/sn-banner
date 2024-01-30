f077f055865eb89ef66928e28b70c8b1eb96cee1
Victor Gaspar <Victor.Gaspar@student.uliege.be>
2024-01-30 21:06:23 +0100

    mask2former refactored

--- models/mask2former/mask2former_r50_1xb2-90k_soccernet-1080x1920.py
+++ models/mask2former/mask2former_r50_1xb2-90k_soccernet-1080x1920.py
@@ -188,17 +188,17 @@ optim_wrapper = dict(
 )
 # learning policy
 param_scheduler = [
-    dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=90000, by_epoch=False)
+    dict(type="PolyLR", eta_min=0, power=0.9, begin=0, end=40000, by_epoch=False)
 ]
 
 # training schedule for 90k
 val_interval = 100
-train_cfg = dict(type="IterBasedTrainLoop", max_iters=90000, val_interval=val_interval)
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=40000, val_interval=val_interval)
 val_cfg = dict(type="ValLoop")
 test_cfg = dict(type="TestLoop")
 default_hooks = dict(
     timer=dict(type="IterTimerHook"),
-    logger=dict(type="LoggerHook", interval=10, log_metric_by_epoch=False),
+    logger=dict(type="LoggerHook", interval=50, log_metric_by_epoch=False),
     param_scheduler=dict(type="ParamSchedulerHook"),
     checkpoint=dict(
         type="CheckpointHook",
--- models/schedule_40k.py
+++ models/schedule_40k.py
@@ -1,24 +1,26 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+optimizer = dict(type="SGD", lr=0.01, momentum=0.9, weight_decay=0.0005)
+optim_wrapper = dict(type="OptimWrapper", optimizer=optimizer, clip_grad=None)
 # learning policy
 param_scheduler = [
-    dict(
-        type='PolyLR',
-        eta_min=1e-4,
-        power=0.9,
-        begin=0,
-        end=40000,
-        by_epoch=False)
+    dict(type="PolyLR", eta_min=1e-4, power=0.9, begin=0, end=40000, by_epoch=False)
 ]
 # training schedule for 40k
-train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=4000)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
+val_interval = 100
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=40000, val_interval=val_interval)
+val_cfg = dict(type="ValLoop")
+test_cfg = dict(type="TestLoop")
 default_hooks = dict(
-    timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=4000),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-    visualization=dict(type='SegVisualizationHook'))
+    timer=dict(type="IterTimerHook"),
+    logger=dict(type="LoggerHook", interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type="ParamSchedulerHook"),
+    checkpoint=dict(
+        type="CheckpointHook",
+        by_epoch=False,
+        save_best="mIoU",
+        interval=val_interval,
+        max_keep_ckpts=2,
+    ),
+    sampler_seed=dict(type="DistSamplerSeedHook"),
+    visualization=dict(type="SegVisualizationHook"),
+)
