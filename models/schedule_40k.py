bd437a5d11c728f4ad152718967f163e3bfa585b
Victor Gaspar <Victor.Gaspar@student.uliege.be>
2024-01-30 21:37:07 +0100

    model ddrnet added

--- models/schedule_40k.py
+++ models/schedule_40k.py
@@ -1,13 +1,15 @@
+iters = 40000
+
 # optimizer
 optimizer = dict(type="SGD", lr=0.01, momentum=0.9, weight_decay=0.0005)
 optim_wrapper = dict(type="OptimWrapper", optimizer=optimizer, clip_grad=None)
 # learning policy
 param_scheduler = [
-    dict(type="PolyLR", eta_min=1e-4, power=0.9, begin=0, end=40000, by_epoch=False)
+    dict(type="PolyLR", eta_min=1e-4, power=0.9, begin=0, end=iters, by_epoch=False)
 ]
 # training schedule for 40k
 val_interval = 100
-train_cfg = dict(type="IterBasedTrainLoop", max_iters=40000, val_interval=val_interval)
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=iters, val_interval=val_interval)
 val_cfg = dict(type="ValLoop")
 test_cfg = dict(type="TestLoop")
 default_hooks = dict(
