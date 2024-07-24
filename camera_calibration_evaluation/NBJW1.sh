e19c83f1c612183e9edbfe579512bd1c75129c96
Victor Gaspar <Victor.Gaspar@student.uliege.be>
2024-07-24 14:35:04 +0200

    NBJW evaluation scrips for NBJW orginan and first modification

---
+++ camera_calibration_evaluation/NBJW1.sh
@@ -0,0 +1,54 @@
+# Credits:
+#
+#   -   https://github.com/mguti97/No-Bells-Just-Whistles/blob/main/scripts/run_pipeline_sn22.sh
+#
+# Description:
+#
+#   -   This script is used to evaluate the original camera calibration model
+#       No Bells Just Whistles (NBJW) on the SoccerNet Game State (sn-gamestate)
+#       dataset.
+
+#!/bin/bash
+
+# Set parameters
+
+# Directory to the sn-gamestate dataset
+ROOT_DIR="../../camera-calibration/sn-gamestate/data/SoccerNetGS"
+# Split to evaluate
+SPLIT="valid"
+# Path to the configuration file for the keypoint detection model
+CFG="../No_Bells_Just_Whistles/config/hrnetv2_w48.yaml"
+# Path to the configuration file for the line detection model
+CFG_L="../No_Bells_Just_Whistles/config/hrnetv2_w48_l.yaml"
+# Path to the weights of the keypoint detection model
+WEIGHTS_KP="../No_Bells_Just_Whistles/SV_kp"
+# Path to the weights of the line detection model
+WEIGHTS_L="../No_Bells_Just_Whistles/SV_lines"
+# Directory to save the inferences (ground truth and predictions)
+SAVE_DIR="inferences1/"
+# Device to use for inference
+DEVICE="cuda:0"
+# Name of the ground truth zip file (pitch annotations from sn-gamestate dataset)
+GT_ZIP_NAME="gt.zip"
+# Name of the predictions zip file (camera parameters predicted by the NBJW model)
+PRED_ZIP_NAME="pred.zip"
+# Version of the NBJW model
+NBJW_VERSION=1
+# Number of concurrent processes to speed up the evaluation
+N_WORKERS=14
+# Set to True to inference and evaluate the 2 first videos only
+TEST="False"
+
+# Run inference script
+if [ $TEST = "True" ]; then
+    python inference_NBJW.py -t --cfg $CFG --cfg_l $CFG_L --weights_kp $WEIGHTS_KP --weights_line $WEIGHTS_L --root_dir $ROOT_DIR --split $SPLIT --save_dir $SAVE_DIR --cuda $DEVICE -v $NBJW_VERSION --gt_zip_name $GT_ZIP_NAME --pred_zip_name $PRED_ZIP_NAME
+else
+    python inference_NBJW.py --cfg $CFG --cfg_l $CFG_L --weights_kp $WEIGHTS_KP --weights_line $WEIGHTS_L --root_dir $ROOT_DIR --split $SPLIT --save_dir $SAVE_DIR --cuda $DEVICE -v $NBJW_VERSION --gt_zip_name $GT_ZIP_NAME --pred_zip_name $PRED_ZIP_NAME
+fi
+
+# Run evaluation script
+if [ $TEST = "True" ]; then
+    python evalai_camera.py -t -s $SAVE_DIR --split $SPLIT --gt_zip_name $GT_ZIP_NAME --pred_zip_name $PRED_ZIP_NAME --workers $N_WORKERS
+else
+    python evalai_camera.py -s $SAVE_DIR --split $SPLIT --gt_zip_name $GT_ZIP_NAME --pred_zip_name $PRED_ZIP_NAME --workers $N_WORKERS
+fi
---
+++ camera_calibration_evaluation/evalai_camera.py
@@ -0,0 +1,179 @@
+# Credits:
+#
+#   -   https://github.com/SoccerNet/sn-calibration/blob/main/src/evalai_camera.py
+#   -   https://github.com/mguti97/No-Bells-Just-Whistles/blob/main/sn_calibration/src/evalai_camera.py
+
+import zipfile
+import argparse
+import numpy as np
+import json
+from tqdm import tqdm
+import sys
+import os
+from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
+
+from traitlets import default
+
+sn_calibration_src_path = os.path.abspath(
+    "../No_Bells_Just_Whistles/sn_calibration/src/"
+)
+sys.path.append(sn_calibration_src_path)
+
+from evaluate_camera import (  # type: ignore
+    get_polylines,
+    scale_points,
+    evaluate_camera_prediction,
+)
+from evaluate_extremities import (  # type: ignore
+    mirror_labels,
+)
+
+
+def evaluate_gt_pred_json(gt_pred_json_tuple):
+    width = 960
+    height = 540
+    gt, prediction = gt_pred_json_tuple
+
+    line_annotations = scale_points(gt, width, height)
+
+    img_groundtruth = line_annotations
+
+    img_prediction = get_polylines(prediction, width, height, sampling_factor=0.9)
+
+    confusion1, _, _ = evaluate_camera_prediction(img_prediction, img_groundtruth, 5)
+
+    confusion2, _, _ = evaluate_camera_prediction(
+        img_prediction, mirror_labels(img_groundtruth), 5
+    )
+
+    accuracy1, accuracy2 = 0.0, 0.0
+    if confusion1.sum() > 0:
+        accuracy1 = confusion1[0, 0] / confusion1.sum()
+
+    if confusion2.sum() > 0:
+        accuracy2 = confusion2[0, 0] / confusion2.sum()
+
+    if accuracy1 > accuracy2:
+        accuracy = accuracy1
+    else:
+        accuracy = accuracy2
+
+    return accuracy
+
+
+def evaluate_zip_dir(
+    zip_dir, gt_zip_name, pred_zip_name, width=960, height=540, workers=1
+):
+    gt_zip = os.path.join(zip_dir, gt_zip_name)
+    prediction_zip = os.path.join(zip_dir, pred_zip_name)
+    gt_archive = zipfile.ZipFile(gt_zip, "r")
+    prediction_archive = zipfile.ZipFile(prediction_zip, "r")
+    gt_jsons = gt_archive.namelist()
+    prediction_jsons = prediction_archive.namelist()
+
+    # Filter out gt jsons that are not in the prediction jsons
+    accuracies = []
+    total_frames = 0
+    missed = 0
+    gt_pred_json_tuples = list()
+    for gt_json in gt_jsons:
+        pred_name = f"camera_{gt_json}"
+
+        total_frames += 1
+
+        if pred_name not in prediction_jsons:
+            missed += 1
+            continue
+
+        prediction = prediction_archive.read(pred_name)
+        prediction = json.loads(prediction.decode("utf-8"))
+        gt = gt_archive.read(gt_json)
+        gt = json.loads(gt.decode("utf-8"))
+        gt_pred_json_tuples.append((gt, prediction))
+
+    dir_name = os.path.basename(zip_dir)
+    tqdm_desc = f"Processing {dir_name}"
+    with Pool(workers) as p:
+        accuracies = list(
+            tqdm(
+                p.imap_unordered(evaluate_gt_pred_json, gt_pred_json_tuples),
+                total=len(gt_pred_json_tuples),
+                leave=False,
+                desc=tqdm_desc,
+            )
+        )
+
+    completeness = (total_frames - missed) / total_frames
+    mean_accuracies = np.mean(accuracies)
+    finalScore = completeness * mean_accuracies
+    return [completeness, mean_accuracies, finalScore]
+
+
+def cpu_count():
+    tmp = os.cpu_count()
+    if tmp is None:
+        return 1
+    return tmp
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluation camera calibration task")
+
+    parser.add_argument(
+        "-s",
+        "--source",
+        type=str,
+        help="Path to the inference folder containing the groundtruth and the predictions per video/sequence",
+        required=True,
+    )
+    parser.add_argument(
+        "--gt_zip_name",
+        type=str,
+        help="Groundtruth zip file name",
+        required=True,
+    )
+    parser.add_argument(
+        "--pred_zip_name",
+        type=str,
+        help="Prediction zip file name",
+        required=True,
+    )
+    parser.add_argument("--split", type=str, required=True, help="Dataset split")
+    parser.add_argument("--width", type=int, default=960)
+    parser.add_argument("--height", type=int, default=540)
+    default_workers = cpu_count() - 2 if cpu_count() > 2 else 1
+    parser.add_argument("--workers", type=int, default=default_workers)
+    parser.add_argument("-t", "--test", action="store_true")
+
+    args = parser.parse_args()
+
+    print("Number of workers:", args.workers)
+
+    directories = os.listdir(os.path.join(args.source, args.split))
+    if args.test:
+        directories.sort()
+        directories = directories[:2]
+
+    zip_dirs = [
+        os.path.join(args.source, args.split, directory) for directory in directories
+    ]
+    results = []
+    for zip_dir in tqdm(zip_dirs, desc="Processing Dirs"):
+        results.append(
+            evaluate_zip_dir(
+                zip_dir,
+                args.gt_zip_name,
+                args.pred_zip_name,
+                args.width,
+                args.height,
+                args.workers,
+            )
+        )
+
+    results = np.array(results).T
+    results = np.mean(results, axis=1)
+    print(f"Results for {args.split}")
+    print(f"Completeness: {results[0]}")
+    print(f"Mean Accuracies: {results[1]}")
+    print(f"Final Score: {results[2]}")
