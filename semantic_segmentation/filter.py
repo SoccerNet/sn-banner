9477658d4e9b0e36e2fd9e612349a71c645a5603
Victor Gaspar <Victor.Gaspar@student.uliege.be>
2024-08-05 20:25:24 +0200

    correction of error when there are no banners in the image

--- semantic_segmentation/filter.py
+++ semantic_segmentation/filter.py
@@ -6,6 +6,8 @@ def keep_biggest_blob(semSegInfPath) -> np.ndarray:
     img = cv2.imread(semSegInfPath, cv2.IMREAD_GRAYSCALE)
     # ? img = np.uint8(img > 0)
     contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) == 0:
+        return img
     largestAreaContour = max(contours, key=cv2.contourArea)
     biggestBlobMask = np.zeros(img.shape, np.uint8)
     cv2.drawContours(biggestBlobMask, [largestAreaContour], 0, 255, cv2.FILLED)  # type: ignore
